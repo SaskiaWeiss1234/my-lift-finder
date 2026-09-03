bca6bf113ff89141c635090311694fc2ae254f36
SaskiaWeiß1234 <saskia.weiss@bstableau.de>
2026-09-03 11:33:03 +0200

    Add freshness check to read route: sync when data is empty or stale

--- src/app/api/elevators/route.js
+++ src/app/api/elevators/route.js
@@ -1,8 +1,16 @@
 import dbConnect from "../../../../db/dbConnect";
 import Elevator from "../../../../db/models/elevators";
+import { syncElevators } from "../../../../db/syncElevators.js";
 
 export async function GET() {
     await dbConnect();
-const elevators = await Elevator.find();
-return Response.json(elevators);
+    //Get newest elevators timestamp
+    const newestElevator = await Elevator.findOne().sort({ lastSyncedAt: -1 });
+    if (!newestElevator || new Date() - newestElevator.lastSyncedAt > 900000) {
+        // If the newest elevator is older than 15 minutes or there are no elevators, sync elevators
+         await syncElevators();
+        }
+        const elevators = await Elevator.find().sort({ lastSyncedAt: -1 });
+        return Response.json(elevators);
+    
 }
